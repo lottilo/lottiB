e6946606758506635322a0f7fa980d7595c0c8b5
lottilo <lora0parvanova@gmail.com>
2025-12-29 09:36:09 +0200

    Problem

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
+app.use(express.json()); // ✅ ДОБАВИ ТОВА (важно!)
+
 /* ---------------------------------------------
    DATABASE CONFIG (Azure SQL)
 --------------------------------------------- */
